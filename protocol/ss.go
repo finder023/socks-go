03979d798c46caa36bcead7baeb07295aace49e4
finder <liuyaolong023@163.com>
2021-06-14 19:19:26 +0800

    fix: fix ss protocol

--- protocol/ss.go
+++ protocol/ss.go
@@ -19,33 +19,30 @@ import (
  *
  */
 
-
 type SSHeaderV4 struct {
-	Ip uint32
+	Ip   uint32
 	Port uint16
 }
 
 type SSProtocol struct {
-	Conn net.Conn
+	Conn   net.Conn
 	Config ProtoConfig
 }
-
 
 func NewSS(conn net.Conn, config ProtoConfig) *SSProtocol {
 	return &SSProtocol{Conn: conn, Config: config}
 }
 
-
 func (p *SSProtocol) parseAddress() (uint8, string, uint16, error) {
-	atypeByte := []byte{0}
+	atypeByte := make([]byte, 1)
 	_, err := p.Conn.Read(atypeByte)
 	if err != nil {
 		return 0, "", 0, nil
 	}
 
-	atype := atypeByte[0]	
+	atype := uint8(atypeByte[0])
 	var addr string
-	var port uint16 
+	var port uint16
 	if atype == 1 {
 		var v4Header SSHeaderV4
 		err = binary.Read(p.Conn, binary.BigEndian, &v4Header)
@@ -56,16 +53,18 @@ func (p *SSProtocol) parseAddress() (uint8, string, uint16, error) {
 		port = v4Header.Port
 	} else if atype == 3 {
 		// domain addr
-		lenByte := []byte{0}
+		lenByte := make([]byte, 1)
 		_, err = p.Conn.Read(lenByte)
 		if err != nil {
 			return 0, "", 0, err
 		}
+
 		domainLen := int(lenByte[0])
-		domainBytes := make([]byte, domainLen + 2) // domain + port
+		domainBytes := make([]byte, domainLen+2) // domain + port
 		_, err = p.Conn.Read(domainBytes)
 		if err != nil {
-			return 0, "", 0, nil }
+			return 0, "", 0, nil
+		}
 		addr = string(domainBytes[:domainLen])
 		port = binary.BigEndian.Uint16(domainBytes[domainLen:])
 	} else {
@@ -73,7 +72,7 @@ func (p *SSProtocol) parseAddress() (uint8, string, uint16, error) {
 	}
 
 	return atype, addr, port, nil
-} 
+}
 
 func (p *SSProtocol) deployLocal(atype uint8, addr string, port uint16) (net.Conn, error) {
 	conn, err := net.Dial("tcp", p.Config.RemoteAddr)
@@ -85,6 +84,8 @@ func (p *SSProtocol) deployLocal(atype uint8, addr string, port uint16) (net.Con
 	addrBytes := make([]byte, 256)
 	xorKey := uint32(time.Now().Unix()) % 256
 	p.Config.Encryptor.XorKey = uint8(xorKey)
+	privateHeader.XorKey = xorKey
+	privateHeader.Port = port
 
 	if atype == 1 {
 		// ip + port
@@ -124,14 +125,13 @@ func (p *SSProtocol) deployServer(atype uint8, addr string, port uint16) (net.Co
 		}
 	}
 
-	var tcpAddr = fmt.Sprintf("%s:%d", ip, port) 
+	var tcpAddr = fmt.Sprintf("%s:%d", ip, port)
 	conn, err := net.Dial("tcp", tcpAddr)
 	if err != nil {
 		return nil, err
 	}
 	return conn, nil
 }
-
 
 func (p *SSProtocol) HandShake() (net.Conn, error) {
 	atype, addr, port, err := p.parseAddress()
@@ -151,4 +151,3 @@ func (p *SSProtocol) HandShake() (net.Conn, error) {
 
 	return remoteConn, nil
 }
-
